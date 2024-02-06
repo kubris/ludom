fb51c28ea56ea3fb4038e6febf9ee4f58487a68e
Andrey Martynov <kubris@mail.ru>
2024-02-06 11:53:36 +0300

    Modify forms and php

--- src/sender.php
+++ src/sender.php
@@ -1,16 +1,14 @@
 <?php
 
-$c = true;
-
 $name    =  trim(htmlentities($_POST['userName']));
-$tel =  trim(htmlentities($_POST['userPhone']));
+$tel 	 =  trim(htmlentities($_POST['userPhone']));
 $text 	 =  trim(htmlentities($_POST['userMessage']));
 $subject =  trim(htmlentities($_POST['subject']));
+$subject =  'Письмо со страницы: ' . $subject;
 
-$to      = "kubris.pro@gmail.com";  // email to receive messages
+$to      = "kubris.pro@gmail.com";
 $date    = date("d.m.Y");
 $time    = date("h:i");
-//$from    = $email;
 $toSelf  = $to;
 
 
@@ -20,23 +18,12 @@ function adopt($txt) {
 
 $headers = "MIME-Version: 1.0" . PHP_EOL .
 "Content-Type: text/html; charset=utf-8" . PHP_EOL .
-'From: '.adopt($subject).'<'.$toSelf .'>' . PHP_EOL;
-//'Reply-To: '.$email.'' . PHP_EOL;
+'From: ' .adopt($subject) . PHP_EOL;
 
-
-// Serialize form fields - that filled-in by User
 foreach ($_POST as $key =>
 $value ) {
-	$msg .= "" 
-		. ( ($c = !$c) ? '	<tr>':'<tr style="background-color: #f8f8f8;">' ) 
-		. 	"<td style='padding: 10px; border: #e9e9e9 1px solid;'><b>$key</b></td>
-			<td style='padding: 10px; border: #e9e9e9 1px solid;'>$value</td>
-			</tr>";
+	$msg .= "$key : $value" . PHP_EOL;
 }
-
-$msg = "<table style='width: 100%;'>"
-	. " $msg"
-	."</table>";
 
 mail($to, $subject, $msg, $headers);
 
